ce651a0e6ef21cf423d2a378e09e1f11b5ffa453
Daniel Lemay <d.s.lemay@icloud.com>
2017-08-14 07:46:38 -0700

    Initial commit

---
+++ src/Components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../style/App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <p>Hello World</p>
+    )
+  }
+}
+
+export default App;
