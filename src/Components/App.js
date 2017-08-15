91a8a4539f83bcbadee64738d8d1354b5a764eca
Daniel Lemay <d.s.lemay@icloud.com>
2017-08-14 17:29:05 -0700

    Add ability to claim a cell
    The functionality was added for handling the user clicking a cell. If
    the cell is not already being controlled, the user's marker will be
    placed on the cell that was clicked and control will shift to the other
    player. If the player clicks on a cell that either they or the opposing
    player already controls no action is taken.

--- src/Components/App.js
+++ src/Components/App.js
@@ -7,8 +7,21 @@ class App extends React.Component {
     super();
 
     this.state = {
-      cellValues: [null,null,null,null,null,null,null,null,null] // Used to track who controls each space.
+      cellValues: ["none","none","none","none","none","none","none","none","none"], // Used to track who controls each space.
+      currPlayer: "X"
     };
+
+    this.playerSelectCell = this.playerSelectCell.bind(this);
+  }
+
+  playerSelectCell(cellValue, cellId) {
+    if (cellValue === "none") {
+       var states = {...this.state};
+       states.cellValues[cellId] = this.state.currPlayer;
+       states.currPlayer = this.state.currPlayer === "X" ? "O" : "X";
+       this.setState({...states});
+    }
+
   }
 
   render() {
@@ -16,19 +29,19 @@ class App extends React.Component {
       <table>
         <tbody>
           <tr>
-            <Cell id="0" />
-            <Cell id="1" />
-            <Cell id="2" />
+            <Cell id="0" cellValue={this.state.cellValues[0]} playerSelectCell={this.playerSelectCell} />
+            <Cell id="1" cellValue={this.state.cellValues[1]} playerSelectCell={this.playerSelectCell} />
+            <Cell id="2" cellValue={this.state.cellValues[2]} playerSelectCell={this.playerSelectCell} />
           </tr>
           <tr>
-            <Cell id="3" />
-            <Cell id="4" />
-            <Cell id="5" />
+            <Cell id="3" cellValue={this.state.cellValues[3]} playerSelectCell={this.playerSelectCell} />
+            <Cell id="4" cellValue={this.state.cellValues[4]} playerSelectCell={this.playerSelectCell} />
+            <Cell id="5" cellValue={this.state.cellValues[5]} playerSelectCell={this.playerSelectCell} />
           </tr>
           <tr>
-            <Cell id="6" />
-            <Cell id="7" />
-            <Cell id="8" />
+            <Cell id="6" cellValue={this.state.cellValues[6]} playerSelectCell={this.playerSelectCell} />
+            <Cell id="7" cellValue={this.state.cellValues[7]} playerSelectCell={this.playerSelectCell} />
+            <Cell id="8" cellValue={this.state.cellValues[8]} playerSelectCell={this.playerSelectCell} />
           </tr>
         </tbody>
       </table>
--- src/Components/Cell.js
+++ src/Components/Cell.js
@@ -4,16 +4,19 @@ import '../style/Cell.css'
 
 class Cell extends React.Component {
   render() {
+    var cellValue = this.props.cellValue !== "none" ? this.props.cellValue : "";
     return (
-      <td id={"cell-" + this.props.id}>
-        {this.props.id}
+      <td id={"cell-" + this.props.id} onClick={() => this.props.playerSelectCell(this.props.cellValue, this.props.id)}>
+        {cellValue}
       </td>
     )
   }
 }
 
 Cell.propTypes = {
-  id: PropTypes.string.isRequired
+  id: PropTypes.string.isRequired,
+  cellValue: PropTypes.string.isRequired,
+  playerSelectCell: PropTypes.func.isRequired
 }
 
 export default Cell;
